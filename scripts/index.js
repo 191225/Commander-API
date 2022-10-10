ccd3d252556b087c7c286e7009a78c154e27bb5d
191225 <191225yuichirou@gmail.com>
2022-10-10 21:59:08 +0900

    add rename

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+import * as Minecraft from "mojang-minecraft";
+import * as MinecraftUI from "mojang-minecraft-ui";
+import tickEvent from "./lib/TickEvent";
+import getScore from "./lib/getScore";
+
+const world = Minecraft.world;
+
+world.events.tick.subscribe(({currentTick, deltaTime}) => {
+    for(let player of world.getPlayers()) {
+        player.getTags().forEach((t) => {
+            t = t.replace(/"/g, "");
+            if (t.startsWith("rename:")) player.rename = t.replace("rename:", "");
+            if (t.startsWith("resetName:")) player.resetName = true;
+        })
+
+        // Rename
+        if (player.rename) {
+            player.rename = player.rename.replace("{name}", player.name);
+            player.nameTag = player.rename;
+            player.rename = false;
+        }
+
+        // Reset name
+        if (player.resetName) {
+            player.nameTag = player.name;
+            player.resetName = false;
+        }
+    }
+})
