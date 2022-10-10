e6a967aa18d9d4d5ea1c8c0003376c8049b0a92c
191225 <93137582+191225@users.noreply.github.com>
2022-10-10 21:36:12 +0900

    start

---
+++ scripts/lib/getScore.js
@@ -0,0 +1,16 @@
+import { world } from "mojang-minecraft";
+
+export function getScore(name, objectName){
+    let scoreboard = world.scoreboard;
+    let ScoreboardObjective = scoreboard.getObjective(objectName);
+    let participants;
+    let participant;
+    let score;
+    try {
+        participants = scoreboard.getParticipants();
+        participant = participants.filter(participant => participant.displayName == name);
+        score = ScoreboardObjective.getScore(participant[0]);
+    } catch {}
+    if (typeof name === "object" && name.nameTag.length) score = ScoreboardObjective.getScore(name.scoreboard);
+    return score;
+}
