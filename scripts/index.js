1b1766bf4b5f1aa214d76a3948f1d0fc127bc913
191225 <93137582+191225@users.noreply.github.com>
2022-10-13 18:02:54 +0900

    update

--- scripts/index.js
+++ scripts/index.js
@@ -16,8 +16,12 @@ import * as Minecraft from "mojang-minecraft";
 import * as MinecraftUI from "mojang-minecraft-ui";
 import tickEvent from "./lib/TickEvent";
 import getScore from "./lib/getScore";
+import { Database, ExtendedDatabase } from "./lib/Database";
+import { setVariable } from "./util";
 
 const world = Minecraft.world;
+
+const Config = new Database("Capi:CONFIG");
 
 world.events.tick.subscribe(({currentTick, deltaTime}) => {
     for(let player of world.getPlayers()) {
@@ -218,20 +222,3 @@ world.events.blockPlace.subscribe(blockPlace => {
     player.runCommandAsync(`scoreboard players set @s Capi:blockPlaceZ ${block.z}`);
 });
 
-function setVariable(player, source) {
-    const dataLength = [...source].filter(t => t === "{").length;
-    for (let i = 0; i < dataLength; i++) {
-        source = source.replace("{name}", player.name);
-        source = source.replace("{nl}", `\n`);
-        try {
-            const score = source.split("{score:")[1].split("}")[0];
-            if (score) source = source.replace(`{score:${score}}`, getScore(player, score));
-        } catch {}
-        try {
-            const tag = source.split("{tag:")[1].split("}")[0];
-            const hasTag = player.getTags().find(t => t.startsWith(tag));
-            if (tag) source = source.replace(`{tag:${tag}}`, hasTag.split(":")[1]);
-        } catch {}
-    }
-    return source;
-}
